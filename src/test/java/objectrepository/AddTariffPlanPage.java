03c0820d0d335d83ede0d1661d987eaffbf750d9
sreenivaslakshmi <pavani0337@gmail.com>
2019-07-24 15:00:52 +0530

    done

--- src/test/java/objectrepository/AddTariffPlanPage.java
+++ src/test/java/objectrepository/AddTariffPlanPage.java
@@ -32,31 +32,8 @@ public class AddTariffPlanPage {
 	
 	
 	
-	/*public AddTariffPlanPage() {
-		PageFactory.initElements(Hooks.driver, this);
-	}
 
-	@FindBy(name = "rental")
-	private WebElement monthRent;
-
-	@FindBy(name = "local_minutes")
-	private WebElement freeloc_min;
-
-	@FindBy(name = "inter_minutes")
-	private WebElement freeinter_min;
-
-	@FindBy(name = "sms_pack")
-	private WebElement freesms;
-
-	@FindBy(name = "minutes_charges")
-	private WebElement localmin_char;
-
-	@FindBy(name = "inter_charges")
-	private WebElement int_char;
-
-	@FindBy(name = "sms_charges")
-	private WebElement sms_char;
-*/	public WebElement getMonthrent() {
+	public WebElement getMonthrent() {
 		return monthrent;
 	}
 	public WebElement getFreelocalMin() {
--- src/test/java/stepDefinition/AddTariffPlanSteps.java
+++ src/test/java/stepDefinition/AddTariffPlanSteps.java
@@ -9,8 +9,9 @@ import objectrepository.AddTariffPlanPage;
 
 public class AddTariffPlanSteps {
 	@When("the user fill in the valid tariff plan details {string},{string},{string},{string},{string},{string},{string}")
-	public void the_user_fill_in_the_valid_tariff_plan_details(String montRent, String freelmin, String freeimin, String freeSmsPack, String localPMin, String interPMin, String smsPerCharges) {
+	public void the_user_fill_in_the_valid_tariff_plan_details(String montRent, String freelmin, String freeimin, String freeSmsPack, String localPMin, String interPMin, String smsPerCharges) throws InterruptedException {
 		AddTariffPlanPage addtirff=new AddTariffPlanPage();
+		Thread.sleep(5000);
 		addtirff.getMonthrent().sendKeys(montRent);
 		addtirff.getFreelocalMin().sendKeys(freelmin);
 		addtirff.getFreeinterMin().sendKeys(freeimin);
